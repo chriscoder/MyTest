bbc43c93a1444d7914b111efe3a2771f109984f6
pengbo <pengbo@onespacechina.com>
2020-12-06 14:18:20 +0800

    测试CRC及子符操作

---
+++ MyTest/CRCTest.cpp
@@ -0,0 +1,53 @@
+﻿// CRCTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <sstream>
+#include "CRC.h"
+using namespace std;
+string unsignedCharToHex(unsigned char ch, string pre = "0x")
+{
+    const std::string hexStr = "0123456789ABCDEF";
+    std::stringstream ss;
+    ss << pre << hexStr[ch >> 4] << hexStr[ch & 0xf] << " ";
+    return ss.str();
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+    unsigned char* src = new unsigned char[263];
+    memset(src, 0xBB, 263);
+    src[0] = 0x55;
+    src[1] = 0xAA;
+    src[2] = 0xFF;
+    src[3] = 0x01;
+    src[4] = 0x03;
+    src[5] = 0x01; 
+
+    unsigned int dwNumOfBytes = (unsigned int)259;
+    unsigned short checksum = CRC::CalCRC16(src + 2, dwNumOfBytes);
+    unsigned char ii[2];
+    memcpy(ii, &checksum, 2);
+    unsigned char i01 = ii[0];
+    unsigned char i02 = ii[1];
+    unsigned char i1 = checksum;
+    unsigned char i2 = checksum >> 8;
+    cout <<"checksum:0x"<< hex << checksum << endl; //输出十六进制数
+    cout << "ii[0]:" << unsignedCharToHex(ii[0]) << endl; //输出十六进制数
+    cout << "ii[1]:" << hex << unsignedCharToHex(ii[1]) << endl; //输出十六进制数
+    cout << "checksum 低八位:" << hex << unsignedCharToHex(i1) << endl; //输出十六进制数
+    cout << "checksum 高八位:" << hex << unsignedCharToHex(i2) << endl; //输出十六进制数
+    system("pause");
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
--- MyTest/CharTest.cpp
+++ MyTest/CharTest.cpp
@@ -1,55 +1,55 @@
-#include<iostream>
-
-using namespace std;
-void testStringCopy();
-void testCopy();
-const char* doCopy();
-int main()
-{ 
-	testCopy();
-	//testStringCopy(); 
-	system("PAUSE ");
-	return 0;
-}
-void testCopy()
-{
-	const char* myStr = doCopy();
-	std:cout << myStr << endl;
-
-	while (*myStr != '\0')
-	{
-		std::cout << *myStr++;  // ++优先级高于*
-	}
-	std::cout << endl;
-}
-const char* doCopy()
-{
-	//const char* c;
-	//string s = "1234";
-	//c = s.c_str(); //c最后指向的内容是垃圾,因为s对象被析构,其内容被处理
-
-	string s = "12345";
-	char* c = new char[s.length() + 1]; 
-	strcpy_s(c, s.length() + 1, s.c_str());
-	return c;
-}
-
-void testStringCopy()
-{
-	string src = "123456";
-	char* cpy_s = new char[src.length() + 1];
-	char cpy_s_c[20];
-	strcpy_s(cpy_s, src.length() + 1, src.c_str());
-	strcpy_s(cpy_s_c, 7, src.c_str());
-
-	char* cpy = new char[src.length() + 1];
-	char cpy_c[20];
-	strcpy(cpy, src.c_str());
-	strcpy(cpy_c, src.c_str());
-
-	std::cout << "cpy_s: " << cpy_s << endl;
-	std::cout << "cpy_s_c: " << cpy_s_c << endl;
-
-	std::cout << "cpy: " << cpy << endl;
-	std::cout << "cpy_c: " << cpy_c << endl;
-}
+//#include<iostream>
+//
+//using namespace std;
+//void testStringCopy();
+//void testCopy();
+//const char* doCopy();
+//int main()
+//{ 
+//	testCopy();
+//	//testStringCopy(); 
+//	system("PAUSE ");
+//	return 0;
+//}
+//void testCopy()
+//{
+//	const char* myStr = doCopy();
+//	std:cout << myStr << endl;
+//
+//	while (*myStr != '\0')
+//	{
+//		std::cout << *myStr++;  // ++优先级高于*
+//	}
+//	std::cout << endl;
+//}
+//const char* doCopy()
+//{
+//	//const char* c;
+//	//string s = "1234";
+//	//c = s.c_str(); //c最后指向的内容是垃圾,因为s对象被析构,其内容被处理
+//
+//	string s = "12345";
+//	char* c = new char[s.length() + 1]; 
+//	strcpy_s(c, s.length() + 1, s.c_str());
+//	return c;
+//}
+//
+//void testStringCopy()
+//{
+//	string src = "123456";
+//	char* cpy_s = new char[src.length() + 1];
+//	char cpy_s_c[20];
+//	strcpy_s(cpy_s, src.length() + 1, src.c_str());
+//	strcpy_s(cpy_s_c, 7, src.c_str());
+//
+//	char* cpy = new char[src.length() + 1];
+//	char cpy_c[20];
+//	strcpy(cpy, src.c_str());
+//	strcpy(cpy_c, src.c_str());
+//
+//	std::cout << "cpy_s: " << cpy_s << endl;
+//	std::cout << "cpy_s_c: " << cpy_s_c << endl;
+//
+//	std::cout << "cpy: " << cpy << endl;
+//	std::cout << "cpy_c: " << cpy_c << endl;
+//}
